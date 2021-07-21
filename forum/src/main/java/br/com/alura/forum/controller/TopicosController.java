6e06e1073df5013eb1dc239651b0b633c092031f
Anderson Passos Silva <anderson.silva@zup.com.br>
2021-07-21 15:52:02 -0300

    Adiciona TopicoRepository

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,6 +1,8 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.dto.TopicoDTO;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.modelo.Curso;
@@ -14,11 +16,16 @@ import java.util.stream.Collectors;
 @Controller
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     @ResponseBody
     public List<TopicoDTO> lista() {
-        Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-
-        return Arrays.asList(topico, topico, topico).stream().map(TopicoDTO::new).collect(Collectors.toList());
+        return this.topicoRepository
+                .findAll()
+                .stream()
+                .map(TopicoDTO::new)
+                .collect(Collectors.toList());
     }
 }
