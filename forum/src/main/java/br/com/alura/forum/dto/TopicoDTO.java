da65eb02f0ad1b5e57edb4da402936726d71d1d0
Anderson Passos Silva <anderson.silva@zup.com.br>
2021-07-21 14:33:43 -0300

    Adiciona modelos, cria controller e DTO Topicos

---
+++ forum/src/main/java/br/com/alura/forum/dto/TopicoDTO.java
@@ -0,0 +1,60 @@
+package br.com.alura.forum.dto;
+
+import br.com.alura.forum.modelo.Topico;
+
+import java.time.LocalDateTime;
+
+public class TopicoDTO {
+    private Long id;
+    private String titulo;
+    private String mensagem;
+
+    private LocalDateTime dataCriacao;
+
+    TopicoDTO() {
+
+    }
+
+    public TopicoDTO(Long id, String titulo, String mensagem, LocalDateTime dataCriacao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.mensagem = mensagem;
+        this.dataCriacao = dataCriacao;
+    }
+
+    public TopicoDTO(Topico topico) {
+        this(topico.getId(), topico.getTitulo(), topico.getMensagem(), topico.getDataCriacao());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+}
