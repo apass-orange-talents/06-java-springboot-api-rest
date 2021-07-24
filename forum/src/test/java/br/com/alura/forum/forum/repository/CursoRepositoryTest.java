2f85d4acd1b3428358106f723dc644e453c38c02
Anderson Passos Silva <anderson.silva@zup.com.br>
2021-07-24 15:29:35 -0300

    Adiciona profile test para ambiente de testes

--- forum/src/test/java/br/com/alura/forum/forum/repository/CursoRepositoryTest.java
+++ forum/src/test/java/br/com/alura/forum/forum/repository/CursoRepositoryTest.java
@@ -6,23 +6,43 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CursoRepositoryTest {
 
     @Autowired
     private CursoRepository cursoRepository;
 
+    @Autowired
+    private TestEntityManager em;
+
     @Test
     public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
         String nomeCurso = "HTML 5";
 
-        Curso curso = cursoRepository.findByNome(nomeCurso);
+        Curso curso = new Curso();
+        curso.setNome(nomeCurso);
+        curso.setCategoria("Front-end");
 
-        Assert.assertNotNull(curso);
-        Assert.assertEquals(nomeCurso, curso.getNome());
+        em.persist(curso);
+
+        Curso cursoRestaurado = cursoRepository.findByNome(nomeCurso);
+        Assert.assertNotNull(cursoRestaurado);
+        Assert.assertEquals(nomeCurso, cursoRestaurado.getNome());
+    }
+
+    @Test
+    public void naoDeveriaCarregarUmCursoCujoNomeNaoEstejaCadastrado() {
+        String nomeCurso = "HTML 5";
+        Curso curso = cursoRepository.findByNome(nomeCurso);
+        Assert.assertNull(curso);
     }
 }
